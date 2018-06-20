f00a8c2f4ea1eb4db61017686f7b5e17b4aa1a18
gurutvakarshan <repository.opensource@gmail.com>
2018-06-21 01:32:59 +0530

    complete function for screenshots of crowling domains

--- googlescraper.py
+++ googlescraper.py
@@ -2,14 +2,15 @@ from GoogleScraper import scrape_with_config, GoogleSearchError
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import re
-
+import imgkit
+import os
 class Scrap():
     def ScrapLinksFromBrowser(self):
     # See in the config.cfg file for possible values
         listoflink=[]
         domain=[]
-        menubar_links=[]
-        dropdown_links=[]
+        menubar_dropdown__links1=[]
+        menubar_dropdown__links2=[]
         config = {
             'use_own_ip': True,
             'keyword': 'security brigade',
@@ -25,7 +26,6 @@ class Scrap():
             'scrape_method': 'selenium',
             'sel_browser': 'chrome',
         }
-
         try:
             search = scrape_with_config(config)
         except GoogleSearchError as e:
@@ -45,12 +45,12 @@ class Scrap():
         for each in listoflink:
             findlinks=re.findall('\w+\.\w+\.\w+',str(each))
             domain.append(findlinks)
-        flat_list = [item for sublist in domain for item in sublist]
+        flat_list = set([item for sublist in domain for item in sublist])
         sum_of_set = set(flat_list)
         con_value = config.get('keyword')
         single_word = "".join(con_value.split())
         data  = "www."+str(single_word)+".com"        
-        if data in sum_of_set:
+        if data in flat_list:
             site_url = Request('http://'+data,headers={'User-Agent': 'Mozilla/5.0'})
             read_site_url = urlopen(site_url).read()
             soup = BeautifulSoup(read_site_url,'lxml')
@@ -59,24 +59,34 @@ class Scrap():
                 find_a = each.find_all("a")
                 for each in find_a:
                     menu_bar_links_level1 = each.get("href")
-                    menubar_links.append(menu_bar_links_level1)
-                    for each in menubar_links:
-                        if each =="/" or each ==" ":
-                            pass
-                        else: 
-                            site_url = Request('http://'+data+each,headers={'User-Agent': 'Mozilla/5.0'})
-                            read_site_url = urlopen(site_url).read()
-                            soup = BeautifulSoup(read_site_url,'lxml')
-                            iter = soup.find_all("ul",{"class":"dropdown-menu"})
+                    menubar_dropdown__links1.append(menu_bar_links_level1)
+                    for each in menubar_dropdown__links1:
+                        site_url = Request('http://'+data+each,headers={'User-Agent': 'Mozilla/5.0'})
+                        read_site_url = urlopen(site_url).read()
+                        soup = BeautifulSoup(read_site_url,'lxml')
+                        iter = soup.find_all("ul",{"class":"dropdown-menu"})
+                        if iter:    
                             for each in iter:
                                 find_a = each.find_all("a")
                                 for each in find_a:
                                     menu_bar_links_level2 = each.get("href")
-                                    dropdown_links.append(menu_bar_links_level2)
-                                    
-                                    sumof_menubar_links = menubar_links + dropdown_links
-                                    print(set(sumof_menubar_links))          
-                   
-x = Scrap()
-x.ScrapLinksFromBrowser()
+                                    menubar_dropdown__links2.append(menu_bar_links_level2)
+                        else:
+                            pass
+        else:
+            pass
+        count = 0                                       
+        setof_final_domain_name = list("http://"+data+each for each in set(menubar_dropdown__links1+menubar_dropdown__links2))
+        # print(setof_final_domain_name)
+        for each in setof_final_domain_name:
+            count = count+1
+            imgkit.from_url(each, str(count)+'.jpg')                        
+
+if __name__ == "__main__":
+    x = Scrap()
+    x.ScrapLinksFromBrowser()
+    # x.Check()
 # x.ScrapLinkWithUrllib()   
+
+
+
