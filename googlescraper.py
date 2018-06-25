832dce49ec4b55726e8624ae5b4c35825165b766
gurutvakarshan <repository.opensource@gmail.com>
2018-06-25 18:11:23 +0530

    class with defs

--- googlescraper.py
+++ googlescraper.py
@@ -3,14 +3,18 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import re
 import imgkit
-import os
-class Scrap():
+import urllib
+
+class Scrap(object):
+    def __init__(self):
+        self.listoflink = []
+        self.flat_list = []
+        self.menubar_dropdown__links1=[]
+        self.menubar_dropdown__links2=[]
+        self.data = None
     def ScrapLinksFromBrowser(self):
     # See in the config.cfg file for possible values
-        listoflink=[]
-        domain=[]
-        menubar_dropdown__links1=[]
-        menubar_dropdown__links2=[]
+        global config
         config = {
             'use_own_ip': True,
             'keyword': 'security brigade',
@@ -41,17 +45,27 @@ class Scrap():
             # print(serp.num_results)
             # ... more attributes ...
             for link in serp.links:
-                listoflink.append(link)
-        for each in listoflink:
+                self.listoflink.append(link)
+
+    def ListOfDomainList(self):
+        domain=[]
+        # VarForListOfLink = self.ScrapLinksFromBrowser()
+        for each in self.listoflink:
             findlinks=re.findall('\w+\.\w+\.\w+',str(each))
             domain.append(findlinks)
-        flat_list = set([item for sublist in domain for item in sublist])
-        sum_of_set = set(flat_list)
+        self.flat_list = set([item for sublist in domain for item in sublist])
+        return self.flat_list
+
+    def ParseDomailLinksFromScraped(self):        
+        sum_of_set = set(self.flat_list)
         con_value = config.get('keyword')
         single_word = "".join(con_value.split())
-        data  = "www."+str(single_word)+".com"        
-        if data in flat_list:
-            site_url = Request('http://'+data,headers={'User-Agent': 'Mozilla/5.0'})
+        self.data  = "www."+str(single_word)+".com"        
+        return self.data
+
+    def AllParsedDomainLinksOfMenuBar(self):    
+        if self.data in self.flat_list:
+            site_url = Request('http://'+self.data,headers={'User-Agent': 'Mozilla/5.0'})
             read_site_url = urlopen(site_url).read()
             soup = BeautifulSoup(read_site_url,'lxml')
             iter = soup.find_all("div",{"class":"collapse navbar-collapse navbar-ex1-collapse"})        
@@ -59,34 +73,44 @@ class Scrap():
                 find_a = each.find_all("a")
                 for each in find_a:
                     menu_bar_links_level1 = each.get("href")
-                    menubar_dropdown__links1.append(menu_bar_links_level1)
-                    for each in menubar_dropdown__links1:
-                        site_url = Request('http://'+data+each,headers={'User-Agent': 'Mozilla/5.0'})
-                        read_site_url = urlopen(site_url).read()
-                        soup = BeautifulSoup(read_site_url,'lxml')
-                        iter = soup.find_all("ul",{"class":"dropdown-menu"})
-                        if iter:    
-                            for each in iter:
-                                find_a = each.find_all("a")
-                                for each in find_a:
-                                    menu_bar_links_level2 = each.get("href")
-                                    menubar_dropdown__links2.append(menu_bar_links_level2)
-                        else:
-                            pass
+                    self.menubar_dropdown__links1.append(menu_bar_links_level1)
+            return self.menubar_dropdown__links1
         else:
-            pass
-        count = 0                                       
-        setof_final_domain_name = list("http://"+data+each for each in set(menubar_dropdown__links1+menubar_dropdown__links2))
-        # print(setof_final_domain_name)
+            pass        
+
+    def AllParsedDomainLinksOfDropDown(self):
+        # MenubarDropdown  = self.AllParsedDomainLinksOfMenuBar()
+        for each in self.menubar_dropdown__links1:
+            site_url = Request('http://'+self.data+each,headers={'User-Agent': 'Mozilla/5.0'})
+            read_site_url = urlopen(site_url).read()
+            soup = BeautifulSoup(read_site_url,'lxml')
+            iter = soup.find_all("ul",{"class":"dropdown-menu"})
+            if iter:    
+                for each in iter:
+                    find_a = each.find_all("a")
+                    for each in find_a:
+                        menu_bar_links_level2 = each.get("href")
+                        self.menubar_dropdown__links2.append(menu_bar_links_level2)
+                return self.menubar_dropdown__links2                
+            else:
+                pass
+
+    def ScreenShotsOfAlllinks(self):
+        # VarForMenuBar = self.AllParsedDomainLinksOfMenuBar()
+        # VarForDomain = self.AllParsedDomainLinksOfDropDown()
+        counter = 0                                       
+        setof_final_domain_name = list("http://"+self.data+each for each in set(self.menubar_dropdown__links1+self.menubar_dropdown__links2))
         for each in setof_final_domain_name:
-            count = count+1
-            imgkit.from_url(each, str(count)+'.jpg')                        
+            print(each)
+            counter = counter+1
+            imgkit.from_url(each, str(counter)+'.jpg')                        
 
 if __name__ == "__main__":
     x = Scrap()
     x.ScrapLinksFromBrowser()
-    # x.Check()
-# x.ScrapLinkWithUrllib()   
-
-
-
+    x.ListOfDomainList()
+    x.ParseDomailLinksFromScraped()
+    x.ScreenShotsOfAlllinks()
+    x.AllParsedDomainLinksOfMenuBar()
+    x.AllParsedDomainLinksOfDropDown()
+    x.ScreenShotsOfAlllinks()
